5ccc41051fc799d3434997ccc46872816c199721
Sebastian Johansson <sebben.97@hotmail.com>
2018-10-04 11:40:26 +0200

    add color button

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hej");
+
+
+function changeColor() {
+
+
+    if (document.body.style.backgroundColor == "greenyellow") {
+        document.body.style.backgroundColor = "white";
+    } else {
+        document.body.style.backgroundColor = "greenyellow";
+    }
+}
