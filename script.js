36f9da0137a89b0cdea4522b107951ad6bd264f2
Sebastian Johansson <sebben.97@hotmail.com>
2018-10-04 13:25:16 +0200

    Random colors RGB.

--- script.js
+++ script.js
@@ -18,3 +18,12 @@ function randomColor() {
     var random = Math.floor(Math.random() * colors.length);
     document.body.style.backgroundColor = colors[random];
 }
+
+function colorGenerator() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var randomColor = "rgb(" + x + "," + y + "," + z + ")";
+    document.body.style.backgroundColor = randomColor;
+    
+}
